ac0cc3387e9a92b3d8ab9f6531d979161789de4a
makej7403-bit <makej7403@gmail.com>
2025-11-16 22:16:11 +0000

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,18 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta
+          name="google-site-verification"
+          content="d8d_1kq-ciq9ByO6gLexUEE9t9snSDd8vwgfnhycVAo"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
